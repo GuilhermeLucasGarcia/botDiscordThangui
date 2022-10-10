035951a472d4a59f10e31aa8d8ee7f590d9723ac
GuilherGarcia <guilherme.garcia@solvers.com.br>
2022-10-09 22:27:48 -0300

    inicio do versionamento

---
+++ commands/comando 1/ping.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "ping", // Coloque o nome do seu comando
+    aliases: [""], // Coloque sinônimos do nome do comando
+
+    run: async(client, message, args) => {
+
+        let embed = new Discord.EmbedBuilder()
+        .setColor("Random")
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL() })
+        .setDescription(` 🥴 Iae ${message.author}, seu ping está em: \`carregando...\`.`);
+
+        let embed2 = new Discord.EmbedBuilder()
+        .setColor("Random")
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL() })
+        .setDescription(` 🥴 Iae ${message.author}, seu ping está em: \`${client.ws.ping}ms\`.`);
+
+        message.reply({ embeds: [embed] }).then(msg => {
+            setTimeout( () => {
+                msg.edit({ embeds: [embed2] })
+            }, 3000)
+        })
+    }
+}
